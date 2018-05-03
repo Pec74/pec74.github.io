780e734f0e171d542fb31d9f1b816de985f5afea
Pec74 <dev.plarage@gmail.com>
2018-05-03 14:06:30 +0200

    Update sw-reg.js

--- docs/sw-reg.js
+++ docs/sw-reg.js
@@ -1,8 +1,8 @@
 //Add this below content to your HTML page, or add the js file to your page at the very top to register sercie worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('sw.js', {
-      scope: './'
+    navigator.serviceWorker.register('service-worker.js', {
+      scope: '././'
     }).then(function(reg) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', reg.scope);
@@ -14,25 +14,25 @@ if ('serviceWorker' in navigator) {
 
   // SW READY
   
-    navigator.serviceWorker.ready
-      .then(function(registration) {
-      // SW is Active
-      console.log('A service worker is active:', registration.active);
+  navigator.serviceWorker.ready
+  .then(function(registration) {
+    console.log('A service worker is active:', registration.active);
+
     // At this point, you can call methods that require an active
     // service worker, like registration.pushManager.subscribe()
-      });
+  });
   
   //SW CONTROLLER
   
    // Do a one-off check to see if a service worker's in control.
-    navigator.serviceWorker.controller) {
-      .then(function(controller) {
+    if (navigator.serviceWorker.controller) {
         console.log('This page is currently controlled by:',
+			navigator.serviceWorker.controller);
       } else {
         console.log('This page is not currently controlled ' +
         'by a service worker.');
       }
-    });
+
 
 } else {
   console.log('Service workers are not supported.');

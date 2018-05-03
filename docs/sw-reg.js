a05273ea26b9a6c6f464a4eb51b537f5afc2e808
Pec74 <dev.plarage@gmail.com>
2018-05-03 13:43:57 +0200

    Update sw-reg.js

--- docs/sw-reg.js
+++ docs/sw-reg.js
@@ -1,8 +1,11 @@
+//Add this below content to your HTML page, or add the js file to your page at the very top to register sercie worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('sw.js', {
+      scope: './'
+    }).then(function(reg) {
       // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      console.log('ServiceWorker registration successful with scope: ', reg.scope);
     }, function(err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);

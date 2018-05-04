6f4b968b6430069118d5faf9fcca941e87f0fe0c
Pec74 <dev.plarage@gmail.com>
2018-05-04 15:29:30 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('video-store').then(function(cache) {
      return cache.addAll([
-       '/pwa-examples/a2hs/index.html'
+       'index.html'
      ]);
    })
  );

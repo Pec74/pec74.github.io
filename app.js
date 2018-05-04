bb785c0330162390fb3e2662b7b2eb1efc55486e
Pec74 <dev.plarage@gmail.com>
2018-05-04 12:06:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('service-worker.js', {
+    navigator.serviceWorker.register('sw.js', {
       scope: '././'
     }).then(function(reg) {
       // Registration was successful

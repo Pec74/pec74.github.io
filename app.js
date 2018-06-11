d19949236fecd2d692f0750154e5e53f8b118d07
Pec74 <dev.plarage@gmail.com>
2018-06-11 12:49:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -20,32 +20,55 @@ if ('serviceWorker' in navigator) {
 
 })();
 
-let installPromptEvent;
 
-window.addEventListener('beforeinstallprompt', (event) => {
-  // Prevent Chrome <= 67 from automatically showing the prompt
-  event.preventDefault();
+var deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', function (e) {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
   // Stash the event so it can be triggered later.
-  installPromptEvent = event;
-  // Update the install UI to notify the user app can be installed
-  //document.querySelector('#install-button').disabled = false;
-    ad2hs-prompt.style.display = 'block';
+  deferredPrompt = e;
+
+  showAddToHomeScreen();
+
 });
- 
-ad2hs-prompt.addEventListener('click', () => {
-  // Update the install UI to remove the install button
-  //document.querySelector('#install-button').disabled = true;
-    ad2hs-prompt.style.display = 'none';
-  // Show the modal add to home screen dialog
-  installPromptEvent.prompt();
+
+function showAddToHomeScreen() {
+
+  var a2hsBtn = document.querySelector(".ad2hs-prompt");
+
+  a2hsBtn.style.display = "block";
+
+  a2hsBtn.addEventListener("click", addToHomeScreen);
+
+}
+
+function addToHomeScreen() {
+
+  var a2hsBtn = document.querySelector(".ad2hs-prompt");
+
+  // hide our user interface that shows our A2HS button
+  a2hsBtn.style.display = 'none';
+
+  // Show the prompt
+  deferredPrompt.prompt();
+
   // Wait for the user to respond to the prompt
-  installPromptEvent.userChoice.then((choice) => {
-    if (choice.outcome === 'accepted') {
-      console.log('User accepted the A2HS prompt');
-    } else {
-      console.log('User dismissed the A2HS prompt');
-    }
-    // Clear the saved prompt since it can't be used again
-    installPromptEvent = null;
-  });
+  deferredPrompt.userChoice
+    .then(function(choiceResult){
+
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+
+      deferredPrompt = null;
+
+    });
+
+}
+
+window.addEventListener('appinstalled', function(evt) {
+  console.log('a2hs', 'installed');
 });

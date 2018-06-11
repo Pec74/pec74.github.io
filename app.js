47283130a4072c03c378055a5d126bce29e254d4
Pec74 <dev.plarage@gmail.com>
2018-06-11 13:24:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -20,24 +20,18 @@ if ('serviceWorker' in navigator) {
 
 })();
 
-let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-});
-
-// Show the prompt
-deferredPrompt.prompt();
-// Wait for the user to respond to the prompt
-deferredPrompt.userChoice
-.then((choiceResult) => {
-  if (choiceResult.outcome === 'accepted') {
-    console.log('User accepted the A2HS prompt');
-  } else {
-    console.log('User dismissed the A2HS prompt');
-  }
-  deferredPrompt = null;
-});
+      window.addEventListener('beforeinstallprompt', function(e) {
+      // beforeinstallprompt Event fired
+      // e.userChoice will return a Promise.
+      e.userChoice.then(function(choiceResult) {
+        console.log(choiceResult.outcome);
+        if(choiceResult.outcome == 'dismissed') {
+          alert('User cancelled home screen install');
+        }
+        else {
+          alert('User added to home screen');
+        }
+      });
+    });
+ 
